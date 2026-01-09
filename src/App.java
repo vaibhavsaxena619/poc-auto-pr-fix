1ea9460dcdd770e0bc6d3fc11643c7368d07874c
VAIBHAV SAXENA <vaibhav.saxena@hp.com>
2026-01-09 12:06:07 +0530

    First error

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+    }
+    public void run() {
+        List<String> users = new ArrayList<>();
     }
 }
