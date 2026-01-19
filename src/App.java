844b9a5371e11b9c7b2550e1515d0fc5009c8d60
VAIBHAV SAXENA <vaibhav.saxena@hp.com>
2026-01-19 18:34:07 +0530

    Testing high confidence

--- src/App.java
+++ src/App.java
@@ -3,37 +3,5 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("Testing Jenkins PR automation!");
-        testMissingImport();
-        testFormatting();
-        testLintIssue();
-        testAssertionError();
-    }
-    
-    public void run() {
-        List<String> users = new ArrayList<>();
-        users.add("test-user");
-        System.out.println("Users: " + users);
-    }
-    
-    // Issue 1: missing_import - undefined class without import
-    public void testMissingImport() {
-        ObjectMapper mapper = new ObjectMapper();
-        System.out.println("Mapper: " + mapper);
-    }
-    
-    // Issue 2: formatting - invalid syntax
-    public void testFormatting(
-        System.out.println("Syntax error - missing closing brace");
-    
-    // Issue 3: lint_issue - unused variable
-    public void testLintIssue() {
-        String unusedVariable = "This is never used";
-        int deadCodeVar = 42;
-        System.out.println("Only printing this");
-    }
-    
-    // Issue 4: test_failure - assertion error
-    public void testAssertionError() {
-        assert false : "This assertion will fail during testing";
     }
 }
