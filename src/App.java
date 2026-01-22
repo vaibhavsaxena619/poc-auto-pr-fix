9f8be71a6d6ce719aa967bc74446212476dfbb03
Build Automation (GPT-5) <build-automation@jenkins.local>
2026-01-22 10:30:52 +0000

    Fix: Add missing imports

--- src/App.java
+++ src/App.java
@@ -1,9 +1,11 @@
+import java.util.Arrays;
+import java.util.List;
 
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("Testing Jenkins PR automation!");
-        List<String> sampleList = List.of("one", "two", "three");
+        List<String> sampleList = Arrays.asList("one", "two", "three");
         for (String item : sampleList) {
             System.out.println(item);
         }
