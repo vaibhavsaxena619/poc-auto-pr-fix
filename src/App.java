f9d7688f9e105199d30c3d3a3db6cc0c7dfd6515
VAIBHAV SAXENA <vaibhav.saxena@hp.com>
2026-01-22 14:56:50 +0530

    Test

--- src/App.java
+++ src/App.java
@@ -3,5 +3,9 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("Testing Jenkins PR automation!");
+        List<String> sampleList = List.of("one", "two", "three");
+        for (String item : sampleList) {
+            System.out.println(item);
+        }
     }
 }
